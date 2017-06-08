f32b411b26453036366b4a0f44fdf89292659f34
SpacePossum <possumfromspace@gmail.com>
2017-06-08 16:25:22 +0200

    Fix trailing common block detection.

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -323,6 +323,19 @@ EOF
                 "a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk",
                 "a\np\nc\nd\ne\nf\ng\nh\ni\nw\nk",
             ],
+            [
+                <<<EOF
+--- Original
++++ New
+@@ @@
+-A
++B
+
+EOF
+            ,
+                "A\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1",
+                "B\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1",
+            ],
         ];
     }
 
@@ -380,5 +393,126 @@ EOL;
         $this->expectExceptionMessageRegExp('#^"to" must be an array or string\.$#');
 
         $this->differ->diffToArray('', new \stdClass);
+    }
+
+    /**
+     * @param array  $expected
+     * @param string $from
+     * @param string $to
+     * @param int    $lineThreshold
+     * @dataProvider provideGetCommonChunks
+     */
+    public function testGetCommonChunks(array $expected, string $from, string $to, int $lineThreshold = 5)
+    {
+        $method = (new \ReflectionObject($this->differ))->getMethod('getCommonChunks');
+        $method->setAccessible(true);
+
+        $result = $method->invoke(
+            $this->differ,
+            $this->differ->diffToArray($from, $to),
+            $lineThreshold
+        );
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function provideGetCommonChunks(): array
+    {
+        return[
+            'same (with default threshold)' => [
+                [],
+                'A',
+                'A',
+            ],
+            'same (threshold 0)' => [
+                [0 => 0],
+                'A',
+                'A',
+                0,
+            ],
+            'empty' => [
+                [],
+                '',
+                '',
+            ],
+            'single line diff' => [
+                [],
+                'A',
+                'B',
+            ],
+            'below threshold I' => [
+                [],
+                "A\nX\nC",
+                "A\nB\nC",
+            ],
+            'below threshold II' => [
+                [],
+                "A\n\n\n\nX\nC",
+                "A\n\n\n\nB\nC",
+            ],
+            'below threshold III' => [
+                [0 => 5],
+                "A\n\n\n\n\n\nB",
+                "A\n\n\n\n\n\nA",
+            ],
+            'same start' => [
+                [0 => 5],
+                "A\n\n\n\n\n\nX\nC",
+                "A\n\n\n\n\n\nB\nC",
+            ],
+            'same start long' => [
+                [0 => 13],
+                "\n\n\n\n\n\n\n\n\n\n\n\n\n\nA",
+                "\n\n\n\n\n\n\n\n\n\n\n\n\n\nB",
+            ],
+            'same part in between' => [
+                [2 => 8],
+                "A\n\n\n\n\n\n\nX\nY\nZ\n\n",
+                "B\n\n\n\n\n\n\nX\nA\nZ\n\n",
+            ],
+            'same trailing' => [
+                [2 => 15],
+                "A\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                "B\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+            ],
+            'same part in between, same trailing' => [
+                [2 => 7, 10 => 16],
+                "A\n\n\n\n\n\n\nA\n\n\n\n\n\n\n",
+                "B\n\n\n\n\n\n\nB\n\n\n\n\n\n\n",
+            ],
+            'below custom threshold I' => [
+                [],
+                "A\n\nB",
+                "A\n\nD",
+                2
+            ],
+            'custom threshold I' => [
+                [0 => 1],
+                "A\n\nB",
+                "A\n\nD",
+                1
+            ],
+            'custom threshold II' => [
+                [],
+                "A\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                "A\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                19
+            ],
+            [
+                [3 => 9],
+                "a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk",
+                "a\np\nc\nd\ne\nf\ng\nh\ni\nw\nk",
+            ],
+            [
+                [0 => 5, 8 => 13],
+                "A\nA\nA\nA\nA\nA\nX\nC\nC\nC\nC\nC\nC",
+                "A\nA\nA\nA\nA\nA\nB\nC\nC\nC\nC\nC\nC",
+            ],
+            [
+                [0 => 5, 8 => 13],
+                "A\nA\nA\nA\nA\nA\nX\nC\nC\nC\nC\nC\nC\nX",
+                "A\nA\nA\nA\nA\nA\nB\nC\nC\nC\nC\nC\nC\nY",
+            ],
+        ];
     }
 }
