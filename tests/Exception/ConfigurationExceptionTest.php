9e5216b7507f140dcd03c83ad8788ff0cd679099
SpacePossum <possumfromspace@gmail.com>
2018-01-25 07:53:36 +0100

    ConfigurationException - previous must be (child of) \Exception, not Exception.

--- tests/Exception/ConfigurationExceptionTest.php
+++ tests/Exception/ConfigurationExceptionTest.php
@@ -17,12 +17,25 @@ use PHPUnit\Framework\TestCase;
  */
 final class ConfigurationExceptionTest extends TestCase
 {
-    public function testConstruct()
+    public function testConstructWithDefaults()
     {
         $e = new ConfigurationException('test', 'A', 'B');
 
         $this->assertSame(0, $e->getCode());
         $this->assertNull($e->getPrevious());
         $this->assertSame('Option "test" must be A, got "string#B".', $e->getMessage());
+    }
+
+    public function testConstruct()
+    {
+        $e = new ConfigurationException(
+            'test',
+            'integer',
+            new \SplFileInfo(__FILE__),
+            789,
+            new \BadMethodCallException(__METHOD__)
+        );
+
+        $this->assertSame('Option "test" must be integer, got "SplFileInfo".', $e->getMessage());
     }
 }
